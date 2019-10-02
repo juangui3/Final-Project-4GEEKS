3ead70c53b70237fc4875efeebf9e74912eb991a
juangui3 <jrein008@fiu.edu>
2019-10-02 01:03:55 +0000

    Login Update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import LogoWhite from "../../img/LogoWhite.png";
 import navbar from "../../styles/navbar.scss";
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Nav, NavDropdown, Form, Button } from "reactstrap";
-
+import { Context } from "../store/appContext";
 export class Navbar extends React.Component {
 	constructor(props) {
 		super(props);
@@ -22,65 +22,77 @@ export class Navbar extends React.Component {
 
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-				<Link className="navbar-brand" to="/home">
-					<img height="50px" src={LogoWhite} />
-				</Link>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon" />
-				</button>
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+							<Link className="navbar-brand" to="/home">
+								<img height="50px" src={LogoWhite} />
+							</Link>
+							<button
+								className="navbar-toggler"
+								type="button"
+								data-toggle="collapse"
+								data-target="#navbarSupportedContent"
+								aria-controls="navbarSupportedContent"
+								aria-expanded="false"
+								aria-label="Toggle navigation">
+								<span className="navbar-toggler-icon" />
+							</button>
 
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav mr-auto">
-						{/*<li className="nav-item active">
+							<div className="collapse navbar-collapse" id="navbarSupportedContent">
+								<ul className="navbar-nav mr-auto">
+									{/*<li className="nav-item active">
 							<a className="nav-link" href="/stock">
 								My Stock
 							</a>
 						</li>*/}
-						<li className="nav-item active">
-							<a className="nav-link" href="/build">
-								Build My Meal
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link" href="/suggestions">
-								Suggestions
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link" href="/history">
-								History
-							</a>
-						</li>
-					</ul>
-				</div>
-				<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-					<DropdownToggle caret>
-						<i className="fas fa-user fa-lg" />
-					</DropdownToggle>
-					<DropdownMenu right>
-						<Link to="/profile">
-							<DropdownItem>My Profile</DropdownItem>
-						</Link>
-						<Link to="/help">
-							<DropdownItem>Help</DropdownItem>
-						</Link>
-						<DropdownItem divider />
-						<DropdownItem>API</DropdownItem>
-						<DropdownItem divider />
-						<Link to="/login">
-							<DropdownItem>Logout</DropdownItem>
-						</Link>
-					</DropdownMenu>
-				</Dropdown>
-			</nav>
+									<li className="nav-item active">
+										<a className="nav-link" href="/build">
+											Build My Meal
+										</a>
+									</li>
+									<li className="nav-item active">
+										<a className="nav-link" href="/suggestions">
+											Suggestions
+										</a>
+									</li>
+									<li className="nav-item active">
+										<a className="nav-link" href="/history">
+											History
+										</a>
+									</li>
+								</ul>
+							</div>
+							<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+								<DropdownToggle caret>
+									<i className="fas fa-user fa-lg" />
+								</DropdownToggle>
+								<DropdownMenu right>
+									<Link to="/profile">
+										<DropdownItem>My Profile</DropdownItem>
+									</Link>
+									<Link to="/help">
+										<DropdownItem>Help</DropdownItem>
+									</Link>
+									<DropdownItem divider />
+									<DropdownItem>API</DropdownItem>
+									<DropdownItem divider />
+									{store.token !== null ? (
+										<Link to="/login">
+											<DropdownItem onClick={() => actions.logout()}>Logout</DropdownItem>
+										</Link>
+									) : (
+										<Link to="/login">
+											<DropdownItem>Login</DropdownItem>
+										</Link>
+									)}
+								</DropdownMenu>
+							</Dropdown>
+						</nav>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,48 +1,83 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import LogoBlack from "../../img/LogoBlack.png";
 import Quote1 from "../../img/Quote1.png";
 import "../../styles/login.scss";
 import { Link } from "react-router-dom";
 
+import { Context } from "../store/appContext";
+import PropTypes from "prop-types";
+
 export class Login extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			email: "",
+			password: ""
+		};
+	}
+	onChangeEmail = e => this.setState({ email: e.target.value });
+	onChangePassword = e => this.setState({ password: e.target.value });
+
 	render() {
 		return (
-			<div className="backlogin">
-				<div className="logo">
-					<img src={LogoBlack} width="300px" />
-				</div>
-				<div className="container cont-login">
-					<div className="row row-login">
-						<p className="login">Login to your account</p>
-						<form>
-							<div className="form-group">
-								<input
-									type="email"
-									className="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-									placeholder="Username"
-								/>
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<React.Fragment>
+							<div className="backlogin">
+								<div className="logo">
+									<img src={LogoBlack} width="300px" />
+								</div>
+								<div className="container cont-login">
+									<div className="row row-login">
+										<p className="login">Login to your account</p>
+										<form>
+											<div className="form-group">
+												<input
+													type="email"
+													className="form-control"
+													id="exampleInputEmail1"
+													aria-describedby="emailHelp"
+													placeholder="Username"
+													onChange={this.onChangeEmail}
+												/>
+											</div>
+											<div className="form-group">
+												<input
+													type="password"
+													className="form-control"
+													id="exampleInputPassword1"
+													placeholder="Password"
+													onChange={this.onChangePassword}
+												/>
+											</div>
+
+											<button
+												type="button"
+												className="btn btn-success btn-login"
+												onClick={() =>
+													actions.onLogin(
+														this.state.email,
+														this.state.password,
+														this.props.history
+													)
+												}>
+												Login
+											</button>
+
+											<p className="forgot">Forgot password? | Sign Up</p>
+										</form>
+									</div>
+								</div>
 							</div>
-							<div className="form-group">
-								<input
-									type="password"
-									className="form-control"
-									id="exampleInputPassword1"
-									placeholder="Password"
-								/>
-							</div>
-							<Link to="/home">
-								<button type="submit" className="btn btn-success btn-login">
-									Login
-								</button>
-							</Link>
-							<p className="forgot">Forgot password?</p>
-						</form>
-					</div>
-				</div>
-			</div>
+						</React.Fragment>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
+Login.propTypes = {
+	history: PropTypes.object
+};
